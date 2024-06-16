ba68b62fa68be1fce7e5c4c82faaabf0acd34097
velosobr <linoc.veloso@gmail.com>
2024-06-15 23:26:49 -0300

    feat: 6.2 Register Screen UI [WIP] - RegisterScreen
    -
    
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- auth/presentation/src/main/java/com/velosobr/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/velosobr/auth/presentation/register/RegisterScreen.kt
@@ -3,18 +3,32 @@
 package com.velosobr.auth.presentation.register
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.velosobr.auth.presentation.R
 import com.velosobr.core.presentation.designsystem.GoRunTheme
+import com.velosobr.core.presentation.designsystem.GorunGray
+import com.velosobr.core.presentation.designsystem.Poppins
 import com.velosobr.core.presentation.designsystem.components.GradientBackground
 import org.koin.androidx.compose.koinViewModel
+import java.time.format.TextStyle
 
 @Composable
 fun RegisterActionRoot(
@@ -24,8 +38,7 @@ fun RegisterActionRoot(
 ) {
 
     RegisterScreen(
-        state = viewModel.state,
-        onAction = viewModel::onAction
+        state = viewModel.state, onAction = viewModel::onAction
 
     )
 
@@ -33,18 +46,58 @@ fun RegisterActionRoot(
 
 @Composable
 private fun RegisterScreen(
-    state: RegisterState,
-    onAction: (RegisterAction) -> Unit
+    state: RegisterState, onAction: (RegisterAction) -> Unit
 ) {
     GradientBackground {
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
                 .fillMaxSize()
-                .weight(1f)
                 .padding(horizontal = 16.dp)
                 .padding(vertical = 32.dp)
+                .padding(top = 16.dp)
         ) {
+            Text(
+                text = stringResource(id = R.string.create_account),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Row {
+                Text(
+                    text = stringResource(id = R.string.already_have_an_account),
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontFamily = Poppins, color = GorunGray
+                    )
+                )
+                val annotatedString = buildAnnotatedString {
+                    pushStringAnnotation(
+                        tag = "clickable_text",
+                        annotation = stringResource(id = R.string.login)
+                    )
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colorScheme.primary,
+                            fontFamily = Poppins
+                        )
+                    ) {
+                        append(stringResource(id = R.string.login))
+                    }
+                }
+                ClickableText(
+                    text = annotatedString,
+                    onClick = { offset ->
+                        annotatedString.getStringAnnotations(
+                            tag = "click_text",
+                            start = offset,
+                            end = offset
+                        ).firstOrNull()?.let {
+                            onAction(RegisterAction.onLoginClick)
+                        }
+
+                    }
+                )
+            }
+            Spacer(modifier = Modifier.height(48.dp))
 
         }
 
@@ -58,9 +111,7 @@ private fun RegisterScreen(
 private fun RegisterActionRootScreenPreview() {
 
     GoRunTheme {
-        RegisterScreen(
-            state = RegisterState(),
-            onAction = {}
+        RegisterScreen(state = RegisterState(), onAction = {}
 
         )
 
