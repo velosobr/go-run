38e21d633d0e3d8077bc82848daee1959bccd828
velosobr <linoc.veloso@gmail.com>
2024-06-17 20:18:20 -0300

    feat: 6.2 Register Screen UI [WIP] - GoRunTextField
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunTextField.kt
+++ core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunTextField.kt
@@ -3,17 +3,35 @@
 package com.velosobr.core.presentation.designsystem.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.foundation.text2.input.TextFieldState
 import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.velosobr.core.presentation.designsystem.CheckIcon
 import com.velosobr.core.presentation.designsystem.EmailIcon
 import com.velosobr.core.presentation.designsystem.GoRunTheme
+import com.velosobr.core.presentation.designsystem.GorunGray
 
 @Composable
 fun GoRunTextField(
@@ -27,7 +45,48 @@ fun GoRunTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     additionalInfo: String? = null,
 ) {
+    var isFocused by remember {
+        mutableStateOf(false)
+    }
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (title != null) {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+            }
+            if (error != null) {
+                Text(
+                    text = error,
+                    color = MaterialTheme.colorScheme.error,
+                    fontSize = 12.sp
+                )
+            } else if (additionalInfo != null) {
+                Text(
+                    text = additionalInfo,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    fontSize = 12.sp
+                )
+            }
+        }
 
+        Spacer(modifier = Modifier.height(4.dp))
+
+        BasicTextField2(
+            state = state,
+            textStyle = LocalTextStyle.current.copy(
+                color = MaterialTheme.colorScheme.onBackground
+            ),
+
+        )
+
+    }
 }
 
 @Preview
