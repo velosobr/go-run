cf437dec6d97787767f1ffc2c223e3636d090961
velosobr <linoc.veloso@gmail.com>
2024-06-18 23:58:42 -0300

    feat: 6.2 Register Screen UI [WIP] - GoRunTextField && RegisterScreen
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunTextField.kt
+++ core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunTextField.kt
@@ -3,15 +3,24 @@
 package com.velosobr.core.presentation.designsystem.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text2.BasicTextField2
+import androidx.compose.foundation.text2.input.TextFieldLineLimits
 import androidx.compose.foundation.text2.input.TextFieldState
 import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,8 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +43,6 @@ import androidx.compose.ui.unit.sp
 import com.velosobr.core.presentation.designsystem.CheckIcon
 import com.velosobr.core.presentation.designsystem.EmailIcon
 import com.velosobr.core.presentation.designsystem.GoRunTheme
-import com.velosobr.core.presentation.designsystem.GorunGray
 
 @Composable
 fun GoRunTextField(
@@ -83,7 +94,74 @@ fun GoRunTextField(
             textStyle = LocalTextStyle.current.copy(
                 color = MaterialTheme.colorScheme.onBackground
             ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground),
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(16.dp))
+                .background(
+                    if (isFocused) {
+                        MaterialTheme.colorScheme.primary.copy(
+                            alpha = 0.05f
+                        )
+                    } else {
+                        MaterialTheme.colorScheme.surface
+                    }
+                )
+                .border(
+                    width = 1.dp,
+                    color = if (isFocused) {
+                        MaterialTheme.colorScheme.primary
+                    } else {
+                        Color.Transparent
+                    },
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(12.dp)
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                },
+            decorator = { innerBox ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    if (startIcon != null) {
+                        Icon(
+                            imageVector = startIcon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                    ) {
+                        if (state.text.isEmpty() && !isFocused) {
+                            Text(
+                                text = hint,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f),
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                        innerBox()
+                    }
+                    if (endIcon != null) {
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Icon(
+                            imageVector = endIcon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                    }
+                }
 
+            }
         )
 
     }
