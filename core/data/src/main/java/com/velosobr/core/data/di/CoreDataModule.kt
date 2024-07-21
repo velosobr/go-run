8ea6790c73c6fc570b06059ae2c7ed0095498716
velosobr <linoc.veloso@gmail.com>
2024-07-20 22:16:20 -0300

    6.7 Setting up the token refresh mechanism
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- core/data/src/main/java/com/velosobr/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/velosobr/core/data/di/CoreDataModule.kt
@@ -5,12 +5,13 @@ import com.velosobr.core.data.auth.EncryptedSessionStorage
 import com.velosobr.core.data.networking.HttpClientFactory
 import com.velosobr.core.domain.SessionStorage
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.scope.get
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
-        HttpClientFactory().build()
+        HttpClientFactory(get()).build()
     }
     singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
