4a5ca00b242bb9a96cc56ca76081c5dff429172f
velosobr <linoc.veloso@gmail.com>
2024-07-31 18:39:07 -0300

    6.10 Splash screen & staying logged in (WIP)
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- app/src/main/java/com/velosobr/gorun/MainActivity.kt
+++ app/src/main/java/com/velosobr/gorun/MainActivity.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.velosobr.core.presentation.designsystem.GoRunTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModel<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.state.isCheckinAuth
+            }
+        }
         setContent {
             GoRunTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/velosobr/gorun/MainState.kt
@@ -0,0 +1,7 @@
+package com.velosobr.gorun
+
+data class MainState(
+    val isLoggedIn: Boolean = false,
+    val isCheckinAuth: Boolean = false
+) {
+}
