574c795700a5ad4df788a6228730f12fff10ee66
velosobr <linoc.veloso@gmail.com>
2024-08-19 23:22:11 -0300

    7.1 Creating the run overview screen skeleton (WIP)
    Signed-off-by: velosobr <linoc.veloso@gmail.com>

--- core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunToolbar.kt
+++ core/presentation/designsystem/src/main/java/com/velosobr/core/presentation/designsystem/components/GoRunToolbar.kt
@@ -2,11 +2,18 @@
 
 package com.velosobr.core.presentation.designsystem.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -15,11 +22,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.velosobr.core.presentation.designsystem.AnalyticsIcon
+import com.velosobr.core.presentation.designsystem.ArrowLeftIcon
 import com.velosobr.core.presentation.designsystem.GoRunTheme
+import com.velosobr.core.presentation.designsystem.GorunBlue
 import com.velosobr.core.presentation.designsystem.LogoIcon
+import com.velosobr.core.presentation.designsystem.Poppins
+import com.velosobr.designsystem.R
 
 @Composable
 fun GoRunToolbar(
@@ -32,21 +49,57 @@ fun GoRunToolbar(
     scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
     startContent: (@Composable () -> Unit)? = null,
 ) {
-    val isDropDownOpen by rememberSaveable { mutableStateOf(false) }
+    var isDropDownOpen by rememberSaveable { mutableStateOf(false) }
 
     TopAppBar(
         title = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    startContent?.invoke()
-                    Spacer(modifier = Modifier.weight(1f))
-                    Text(
-                        text = title,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                startContent?.invoke()
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontFamily = Poppins
+                )
+            }
         },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent
+        ),
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = ArrowLeftIcon,
+                        contentDescription = stringResource(id = R.string.go_back),
+                        tint = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier
+                            .size(35.dp)
+                    )
+
+                }
+            }
+        },
+        actions = {
+            if (menuItems.isNotEmpty()) {
+                Box {
+                    IconButton(onClick = {
+                        isDropDownOpen = true
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = stringResource(id = R.string.menu),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    }
+                }
+            }
+        }
     )
 
 }
@@ -56,12 +109,25 @@ fun GoRunToolbar(
 private fun GoRunToolbarPreview() {
     GoRunTheme {
         GoRunToolbar(
-            title = "Title",
+            title = "GoRun",
             showBackButton = false,
             modifier = Modifier.fillMaxWidth(),
             startContent = {
-                Icon(imageVector = LogoIcon, contentDescription = null)
-            }
+                Icon(
+                    imageVector = LogoIcon,
+                    contentDescription = null,
+                    tint = GorunBlue,
+                    modifier = Modifier
+                        .size(33.dp)
+                )
+            },
+            menuItems = listOf(
+                DropDownMenuItem(
+                    icon = AnalyticsIcon,
+                    title = "Analytics"
+                )
+
+            )
         )
     }
 }
